fbfa3cad3c833a4fdd17778f9b1de30ef558bbbc
mario camayo <mariocamayo17@gmail.com>
2024-08-28 02:43:34 -0500

    se agregó el archivo service_data, el contenido de la sección services y los estilos css

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,7 +1,27 @@
+import  {services_data} from '../../assets/services_data.js'
 import './services.css'
+
 const Services = () => {
   return (
-    <div>Services</div>
+    <div className='services'>
+       <div className="services-title">
+        <h1>My Services</h1>
+        <img src="" alt="" />
+       </div>
+       <div className="services-container">
+          {services_data.map((service, index)=>{
+             return <div key={index} className='services-format'>
+              <h3> {service.s_no} </h3>
+              <h2> {service.s_name} </h2>
+              <p> {service.s_desc} </p>
+              <div className='services-readmore'>
+                <p>Read More</p>
+                <img src="" alt="" />
+              </div>
+            </div>
+          })}
+       </div>
+    </div>
   )
 }
 
