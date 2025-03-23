9a1a18b409853387a194b9a653e791cdcde72ec7
mario camayo <mariocamayo17@gmail.com>
2025-03-22 22:53:05 -0500

    se modificó el componente About.jsx, NavBar.jsx y MyWork.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -36,7 +36,6 @@ const Contact = () => {
     <div id="contacto" className="contact">
       <div className="contact-title">
         <h1>Contáctame</h1>
-        {/* <img src= {logo_menu} alt="logo_menu" /> */}
       </div>
       <div className="contact-section">
         <div className="contact-left">
